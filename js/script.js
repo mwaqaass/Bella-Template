91ed4d1541e8cb9a060f97e73bca9b2b564c8172
Waqas <33598165+mwaqaass@users.noreply.github.com>
2019-10-18 01:03:02 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+  $(document).ready(function() {
+    $("#reasons").mouseover(function() {
+      $(".read-more").toggleClass("animated");
+    });
+  });
+
+  $(document).ready(function() {
+    $("#reasons2").mouseover(function() {
+      $(".read-more2").toggleClass("animated");
+    });
+  });
+
+  $(document).ready(function() {
+    $("#reasons3").mouseover(function() {
+      $(".read-more3").toggleClass("animated");
+    });
+  });
+
+  $(document).ready(function() {
+    $("#reasons4").mouseover(function() {
+      $(".reload").toggleClass("animated");
+    });
+  });
+
+$(document).ready(function() {
+    $("#reasons5").mouseover(function() {
+      $(".reload2").toggleClass("animated");
+    });
+  });
+
+$(document).ready(function() {
+    $("#reasons6").mouseover(function() {
+      $(".reload3").toggleClass("animated");
+    });
+  });
+
+$(document).ready(function() {
+    $("#button-pink").mouseover(function() {
+      $(".btn-pink").toggleClass("animated");
+    });
+  });
